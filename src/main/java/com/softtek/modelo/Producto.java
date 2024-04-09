612ae36637afc3378052b5161b20539d19895f9e
Ángel Gallardo <angelgallardoturpin@gmail.com>
2024-04-09 09:35:57 +0200

    Initial commit

---
+++ src/main/java/com/softtek/modelo/Producto.java
@@ -0,0 +1,20 @@
+package com.softtek.modelo;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Data
+public final class Producto {
+    public static int CONTADOR=0;
+    private final int ID;
+    private String nombre;
+    private double precio;
+
+    public Producto() {
+        this.ID = ++CONTADOR;
+    }
+
+    public final double importe(int cantidad) {
+        return cantidad*precio;
+    }
+}
